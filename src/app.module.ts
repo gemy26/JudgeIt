10d88280ce4498d27f03f48d8bd212197eb5da9a
gemy26 <ahmed.gamal.5511@icloud.com>
2026-03-28 23:06:20 +0200

    refactor: move global config from main.ts into AppModule
    - register ValidationPipe globally via APP_PIPE
    - register cookieParser middleware via MiddlewareConsumer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  ValidationPipe,
+} from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
@@ -6,12 +11,13 @@ import { UsersModule } from './users/users.module';
 import { SubmissionsModule } from './submissions/submissions.module';
 import { ProblemsModule } from './problems/problems.module';
 import { EmailModule } from './email/email.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { AtGuard, RolesGuard } from './common/guards';
 import { KafkaModule } from './kafka/kafka.module';
 import { ExecutionModule } from './execution/execution.module';
 import { JudgeModule } from './judge/judge.module';
 import { WorkerModule } from './worker/worker.module';
+import cookieParser from 'cookie-parser';
 
 @Module({
   imports: [
@@ -39,6 +45,17 @@ import { WorkerModule } from './worker/worker.module';
       provide: APP_GUARD,
       useClass: RolesGuard,
     },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      }),
+    },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieParser()).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
-import cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -28,21 +26,13 @@ async function bootstrap() {
     }),
   });
 
-
-  const config = new DocumentBuilder().setTitle('JudgeIt API').setVersion('1.0').build();
+  const config = new DocumentBuilder()
+    .setTitle('JudgeIt API')
+    .setVersion('1.0')
+    .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory());
-
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     transform: true
-  //   })
-  // )
-
-  app.use(cookieParser());
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
